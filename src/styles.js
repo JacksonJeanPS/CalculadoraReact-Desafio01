96e9edf532c13cf0721007426e40cfdcd2494b49
JacksonJeanPS <jacksonjean01@gmail.com>
2022-10-26 17:05:22 -0300

    calc pronta
    Calc pronta

---
+++ src/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+	height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+`
+
+
+export const Content = styled.div`
+    background-color: #FFFFFF;
+    width: 50%;
+    border: 15px  groove  rgb(131,111,255);
+`
+
+export const Row = styled.div`
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const Column = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`
+
